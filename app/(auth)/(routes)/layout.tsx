be17c9a21d2f8bb6881a2127f6f641d839c5f01c
mahbub.mohsin <mollahmahbub81@gmail.com>
2023-10-24 00:10:43 +0600

    one

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,12 @@
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <div className=" h-full bg-red-500 text-white">
+        {children}
+      </div>
+    )
+  }
+  
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers:[
+        {
+            domain:"https://normal-grouper-59.clerk.accounts.dev",
+            applicationID: "convex",
+        }
+    ]
+}
